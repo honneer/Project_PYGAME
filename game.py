4bbcf23092b82b8bdcd4450ef5a1de21f4bfcc3d
honneer <131390915+honneer@users.noreply.github.com>
2025-08-06 09:47:50 +0530

    project game using python and pygame library

--- game.py
+++ game.py
@@ -4,7 +4,6 @@
 #event.key (not type as we r letting uder press key and checking that) & forgot to add_change - playerX_change  
 #didn't create the variable palerX_change
 
-
 import pygame
 
 #initialization 
@@ -12,6 +11,8 @@ pygame.init()
 
 #screen width and height
 screen = pygame.display.set_mode((800, 600))
+#window's name
+pygame.display.set_caption("TinyTails Game")
 
 #playerimg and x and y
 playerImg = pygame.image.load('bear.png')
@@ -39,13 +40,13 @@ while running:
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                playerX_change = -0.3 
+                playerX_change = -0.4 
             if event.key == pygame.K_RIGHT:
-                playerX_change = 0.3
+                playerX_change = 0.4
             if event.key == pygame.K_UP:
-                playerY_change = -0.3
+                playerY_change = -0.4
             if event.key == pygame.K_DOWN:
-                playerY_change = 0.3
+                playerY_change = 0.4
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 playerX_change = 0
@@ -53,7 +54,15 @@ while running:
                 playerY_change = 0
 
     playerX += playerX_change
+    if playerX <= 0: 
+        playerX = 0 
+    elif playerX >= 736: #800-64
+        playerX = 736
     playerY += playerY_change
+    if playerY <= 0:
+        playerY = 0
+    elif playerY >= 536: # 600-64
+        playerY = 536
 
     player(playerX, playerY)
     pygame.display.update()
